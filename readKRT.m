8a74175aa13fa25a6c9ca6befbceb77434a70860
unknown <cyanidesd@gmail.com>
2017-12-09 20:16:39 -0500

    first

---
+++ readKRT.m
@@ -0,0 +1,21 @@
+function [K, R, t] = readKRT(filename)
+fidin = fopen(filename);
+R = zeros(3, 3);
+t = zeros(1, 3);
+K = zeros(3, 3);
+for i = 1:3
+    tline = fgetl(fidin);
+    K(i, :) = str2num(tline);
+end
+tline = fgetl(fidin);
+
+for i = 1:3
+    tline = fgetl(fidin);
+    R(i, :) = str2num(tline);
+end
+
+tline = fgetl(fidin);
+t = str2num(tline);
+t = t';
+
+end
